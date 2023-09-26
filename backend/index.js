6f97b4370c3797be9ea42481a10d3e82886a77a3
Rahultej05N <rahultejnedunuri@gmail.com>
2023-09-26 23:23:31 +0530

    a

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,10 @@ const express = require('express')
 const app = express()
 const port = 5000
  const path=require('path');
+ const cors = require('cors');
 
+ app.use(cors());
+ 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header(
